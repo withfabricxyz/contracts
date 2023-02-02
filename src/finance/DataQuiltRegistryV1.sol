dedbe5a43ca515a2ca3983e8d7c55cad85e15227
Dan Simpson <dan.simpson@gmail.com>
2023-02-02 11:17:55 -0800

    Add DataQuiltRegistry NFT contract

---
+++ src/finance/DataQuiltRegistryV1.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ERC20CrowdFinancingV1.sol";
+import "./EthCrowdFinancingV1.sol";
+
+/**
+ *
+ * @title DataQuilt NFT Contract for Fabric Campaigns
+ * @author Dan Simpson
+ *
+ * ERC721 + Metadata, with custom minting functions for users who have contributed to
+ * a Fabric campaign.
+ *
+ */
+contract DataQuiltRegistryV1 is ERC721 {
+    // Base URI for which we generate URIs for NFTs
+    string private _baseUri;
+
+    // Campaign specific double mint guard
+    mapping(address => mapping(address => bool)) private _campaignMints;
+
+    /**
+     * @param name the name of the token
+     * @param symbol the symbol of hte token
+     * @param baseUri the base URI, such as: `https://somehost.com/`
+     */
+    constructor(string memory name, string memory symbol, string memory baseUri) ERC721(name, symbol) {
+        _baseUri = baseUri;
+    }
+
+    /**
+     * Mint a contribution token using a tokenId, which encodes the campaign address + encoded variant
+     *
+     * @param tokenId the id of the token, which is an abi packed [campaign address, uint64(0), uint32(variant)]
+     */
+    function mintContributionToken(uint256 tokenId) external {
+        address account = msg.sender;
+        address campaignAddress = address(uint160(tokenId >> 96));
+        require(!_campaignMints[campaignAddress][account], "Err: 101, already minted");
+        // We can use the same contract wrapper, since funtion signature is identical
+        // We should probably have another assertion in the future, verifiying this is in
+        // fact a CF contract... as someone could spoof this. That said, the rendering would
+        // be incorrect
+        EthCrowdFinancingV1 campaign = EthCrowdFinancingV1(payable(campaignAddress));
+        require(campaign.depositedAmount(account) > 0, "Err: 100, No deposits registered");
+        _safeMint(account, tokenId);
+        _campaignMints[campaignAddress][account] = true;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;
+    }
+}
---
+++ test/finance/DataQuiltRegistryV1.t.sol
@@ -0,0 +1,66 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@forge/Test.sol";
+import "@forge/console2.sol";
+import "./util/TestHelper.sol";
+import "src/finance/ERC20CrowdFinancingV1.sol";
+import "src/tokens/ERC20Token.sol";
+import "src/finance/EthCrowdFinancingV1.sol";
+import "src/finance/DataQuiltRegistryV1.sol";
+
+contract DataQuiltRegistryV1Test is TestHelper {
+    DataQuiltRegistryV1 internal registry;
+
+    function generateId(address addr, uint32 variant) public returns (uint256) {
+        return uint256(bytes32(abi.encodePacked(addr, uint64(0), variant)));
+    }
+
+    function setUp() public {
+        registry = new DataQuiltRegistryV1("TEST", "TEST", "https://art.meow.com/");
+        deal(depositor, 1e19);
+    }
+
+    function testInvalidAddress() public {
+        uint256 id = generateId(beneficiary, uint32(0xff));
+        vm.expectRevert();
+        registry.mintContributionToken(id);
+    }
+
+    function testMintWithoutDeposits() public {
+        EthCrowdFinancingV1 cf = createETHCampaign();
+        uint256 id = generateId(address(cf), uint32(0xff));
+        vm.expectRevert("Err: 100, No deposits registered");
+        registry.mintContributionToken(id);
+    }
+
+    function testMintWithDeposits() public {
+        EthCrowdFinancingV1 cf = createETHCampaign();
+        uint256 id = generateId(address(cf), uint32(0xff));
+
+        depositEth(cf, depositor, 1e18);
+        vm.startPrank(depositor);
+        registry.mintContributionToken(id);
+
+        assertEq(1, registry.balanceOf(depositor));
+        assertEq(
+            "https://art.meow.com/20868766820000560018335248259782891309437497197130732538605701684905228894463",
+            registry.tokenURI(id)
+        );
+
+        vm.expectRevert("Err: 101, already minted");
+        registry.mintContributionToken(id);
+        vm.expectRevert("Err: 101, already minted");
+        registry.mintContributionToken(id + 1);
+    }
+
+    function testERC20MintWithDeposits() public {
+        ERC20CrowdFinancingV1 cf = createERC20Campaign();
+        uint256 id = generateId(address(cf), uint32(0xff));
+        dealTokens(ERC20Token(cf.tokenAddress()), depositor, 1e18);
+        depositTokens(cf, depositor, 1e18);
+        vm.startPrank(depositor);
+        registry.mintContributionToken(id);
+        assertEq(1, registry.balanceOf(depositor));
+    }
+}
