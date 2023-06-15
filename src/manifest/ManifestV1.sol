45368c675af8fb5fd680800df5ffff5e044468e6
Dan Simpson <dan.simpson@gmail.com>
2023-06-15 10:50:20 -0700

    KISS for now.

--- src/manifest/ManifestV1.sol
+++ src/manifest/ManifestV1.sol
@@ -2,59 +2,72 @@
 
 pragma solidity 0.8.17;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin-upgradeable/contracts/access/OwnableUpgradeable.sol";
 import "@openzeppelin-upgradeable/contracts/token/ERC721/ERC721Upgradeable.sol";
 import "@forge/console2.sol";
 
 contract ManifestV1 is ERC721Upgradeable, OwnableUpgradeable {
+    using SafeERC20 for IERC20;
 
+    /// @dev Emitted when the owner withdraws available funds
+    event CreatorWithdraw(address indexed account, uint256 tokensTransferred);
 
-    // We need to support a baseUri for the tokenURI
-    // Maybe: We should support mint on behalf of (so we could support monthly credit card payments, etc)
-    // We need to allow the creator to update the cost of time (maybe, if this isn't terribly hard)
-    // We should allow pausing of subscriptions
-    // Support ERC20 and ETH
-    // Allowances for ERC20
-    // revert on receive
+    /// @dev Emitted when a subscriber purchases additional time
+    event SubscriptionFunded(address indexed account, uint256 tokensTransferred, uint256 timeRemaining);
 
+    /// @dev Emitted when a subscriber cancels and reclaims their remaining time and tokens
+    event SubscriptionCanceled(address indexed account, uint256 tokensTransferred, uint256 timeReclaimed);
+
+    // The subscription struct which holds the state of a subscription for an account
     struct Subscription {
-      uint256 tokenId;
-      uint256 commited;
-      uint64 credits;
-      uint64 start;
+        uint256 tokenId;
+        uint256 secondsPurchased;
+        uint256 timeOffset;
     }
 
-    // we need something that allows for fast computation of funds streamed
-
+    // The cost of one second in denominated token (wei or other base unit)
     uint256 private _tokensPerSecond;
-
     uint256 private _withdrawn;
-
-    uint256 private _committed;
-    uint256 private _time;
-    uint256 private _ratio;
-
     uint256 private _tokenCounter;
-
     string private _baseUri;
+
+    IERC20 private _token;
+    bool private _erc20;
 
     // IERC20 private _token;
 
     mapping(address => Subscription) private _subscriptions;
 
     constructor() {
-      _disableInitializers();
+        _disableInitializers();
     }
 
-    function initialize(address creator, string memory baseUri, uint256 tokensPerSecond) public initializer {
-      __ERC721_init("Manifest", "MANIFEST");
-      _baseUri = baseUri;
-      _tokensPerSecond = tokensPerSecond;
-      _transferOwnership(creator);
+    // TODO: Native Token Guard
+    receive() external payable {
+        _purchase(msg.sender, msg.value);
     }
 
-    function purchaseFor(address account, uint256 amount) external payable {
+    function initialize(
+        string memory name,
+        string memory symbol,
+        string memory baseUri,
+        address owner,
+        uint256 tokensPerSecond,
+        address erc20TokenAddr
+    ) public initializer {
+        __ERC721_init(name, symbol);
+        _transferOwnership(owner);
+        _baseUri = baseUri;
+        _tokensPerSecond = tokensPerSecond;
+        _token = IERC20(erc20TokenAddr);
+        _erc20 = erc20TokenAddr != address(0);
     }
+
+    /////////////////////////
+    // Consumer Calls
+    /////////////////////////
 
     function purchase(uint256 amount) external payable {
         address account = msg.sender;
@@ -62,96 +75,70 @@ contract ManifestV1 is ERC721Upgradeable, OwnableUpgradeable {
         _purchase(account, amount);
     }
 
-    function withdraw() external payable onlyOwner {
-      uint256 balance = creatorBalance();
-      require(balance > 0, "No Balance");
-      _withdrawn += balance;
-      (bool sent,) = payable(msg.sender).call{value: balance}("");
-      require(sent, "Failed to transfer Ether");
-    }
+    function _purchase(address account, uint256 amount) internal {
+        Subscription memory sub = _subscriptions[account];
 
-    function creatorEarnings() public view returns (uint256) {
-      uint256 value = 0;
-      for(uint256 i = 1; i <= _tokenCounter; i++) {
-        address account = _ownerOf(i);
-        value += (_subscriptions[account].credits - balanceOf(account));
-      }
-      return value * _tokensPerSecond;
-    }
+        uint256 tv = timeValue(amount);
+        uint256 time = block.timestamp;
 
-    function creatorBalance() public view returns (uint256) {
-      return creatorEarnings() - _withdrawn;
+        if (sub.tokenId == 0) {
+            sub = Subscription(_nextTokenId(), tv, time);
+            _subscriptions[account] = sub;
+            _safeMint(account, sub.tokenId);
+        } else {
+            if (time > sub.timeOffset + sub.secondsPurchased) {
+                sub.timeOffset = time - sub.secondsPurchased;
+            }
+            sub.secondsPurchased += tv;
+            _subscriptions[account] = sub;
+        }
+        emit SubscriptionFunded(account, amount, sub.secondsPurchased - sub.timeOffset);
     }
-
-    /////////////////////////
-    // Cancellation
-    /////////////////////////
 
     function cancelSubscription(uint256 tokenId) external payable {
-      _cancel(_ownerOf(tokenId));
+        _cancel(_ownerOf(tokenId));
     }
 
     function cancelSubscription() external {
-      _cancel(msg.sender);
+        _cancel(msg.sender);
     }
 
     function _cancel(address account) internal {
-      uint256 balance = balanceOf(account);
-      require(balance > 0, "NoActiveSubscription");
+        uint256 balance = timeBalanceOf(account);
+        require(balance > 0, "NoActiveSubscription");
+        _subscriptions[account].secondsPurchased -= balance;
 
-      // TODO: This broken for price changes
-      _subscriptions[account].credits -= uint64(balance);
-      uint256 refund = balance * _tokensPerSecond;
-      // _committed -= refund;
-      // _committedTime -= balance;
-      (bool sent,) = payable(account).call{value: refund}("");
-      require(sent, "Failed to transfer Ether");
+        uint256 tokens = balance * _tokensPerSecond;
+
+        emit SubscriptionCanceled(account, tokens, balance);
+        (bool sent,) = payable(account).call{value: tokens}("");
+        require(sent, "Failed to transfer Ether");
     }
 
-    function pause() external onlyOwner {
-      // pause purchases
+    /////////////////////////
+    // Creator Calls
+    /////////////////////////
+
+    // withdrawEarnings?
+    function withdraw() external payable onlyOwner {
+        uint256 balance = creatorBalance();
+        require(balance > 0, "No Balance");
+        _withdrawn += balance;
+
+        emit CreatorWithdraw(msg.sender, balance);
+        (bool sent,) = payable(msg.sender).call{value: balance}("");
+        require(sent, "Failed to transfer Ether");
     }
 
-    function updatePrice(uint256 tokensPerSecond) external onlyOwner {
-        _tokensPerSecond = tokensPerSecond;
-    }
+    function pausePurchases() external onlyOwner {}
+
+    // function setTokensPerSecond(uint256 tokensPerSecond) external onlyOwner {
+    //     _tokensPerSecond = tokensPerSecond;
+    // }
 
     function _nextTokenId() internal returns (uint256) {
         _tokenCounter += 1;
         return _tokenCounter;
-    }
-
-    function _purchase(address account, uint256 amount) internal {
-        Subscription memory sub = _subscriptions[account];
-
-        uint64 tv = timeValue(amount);
-        uint64 time = uint64(block.timestamp);
-
-        if(sub.tokenId == 0) {
-          sub = Subscription(_nextTokenId(), amount, tv, time);
-          _subscriptions[account] = sub;
-          _safeMint(account, sub.tokenId);
-        } else {
-          // NAIVE! TODO (Deal with multiple price points)
-          // TODO: More tests
-          if(time > sub.start + sub.credits) {
-            sub.start = time - sub.credits;
-          }
-
-          sub.commited += amount;
-          sub.credits += tv;
-          _subscriptions[account] = sub;
-        }
-
-        // TODO:
-        // _committed += amount;
-        // _time += tv;
-        // _ratio += (amount / _committed) / _time;
-        // slide time
-    }
-
-    function timeValue(uint256 amount) public view returns (uint64) {
-        return uint64(amount / _tokensPerSecond);
     }
 
     /**
@@ -161,59 +148,52 @@ contract ManifestV1 is ERC721Upgradeable, OwnableUpgradeable {
         return _baseUri;
     }
 
+    ////////////////////////
+    // Informational
+    ////////////////////////
+
+    function timeValue(uint256 amount) public view returns (uint256) {
+        return amount / _tokensPerSecond;
+    }
+
+    function creatorBalance() public view returns (uint256) {
+        return creatorEarnings() - _withdrawn;
+    }
+
+    function creatorEarnings() public view returns (uint256) {
+        uint256 value = 0;
+        for (uint256 i = 1; i <= _tokenCounter; i++) {
+            address account = _ownerOf(i);
+            value += (_subscriptions[account].secondsPurchased - timeBalanceOf(account));
+        }
+        return value * _tokensPerSecond;
+    }
+
     //////////////////////
     // Overrides
     //////////////////////
 
-    function ownerOf(uint256 tokenId) public view override returns (address) {
-        address owner = _ownerOf(tokenId);
-        require(owner != address(0) && balanceOf(owner) > 0, "ERC721: invalid token ID");
-        return owner;
-    }
-
+    // balanceOf is the number of tokens at time of call
     function balanceOf(address account) public view override returns (uint256) {
-      Subscription memory sub = _subscriptions[account];
-      uint256 expiresAt = sub.start + sub.credits;
-      if(expiresAt <= block.timestamp) {
-        return 0;
-      }
-      return expiresAt - block.timestamp;
+        return timeBalanceOf(account) * _tokensPerSecond;
     }
 
-    function subscriptionOf(address account) public view returns (uint256 tokenId, uint256 commited, uint64 credits, uint64 start) {
-      Subscription memory sub = _subscriptions[account];
-      return (sub.tokenId, sub.commited, sub.credits, sub.start);
+    // The number of seconds remaining on the subscription for an account
+    function timeBalanceOf(address account) public view returns (uint256) {
+        Subscription memory sub = _subscriptions[account];
+        uint256 expiresAt = sub.timeOffset + sub.secondsPurchased;
+        if (expiresAt <= block.timestamp) {
+            return 0;
+        }
+        return expiresAt - block.timestamp;
     }
 
+    function subscriptionOf(address account)
+        public
+        view
+        returns (uint256 tokenId, uint256 secondsPurchased, uint256 timeOffset)
+    {
+        Subscription memory sub = _subscriptions[account];
+        return (sub.tokenId, sub.secondsPurchased, sub.timeOffset);
+    }
 }
-
-// interface IERC5643 {
-//     /// @notice Emitted when a subscription expiration changes
-//     /// @dev When a subscription is canceled, the expiration value should also be 0.
-//     event SubscriptionUpdate(uint256 indexed tokenId, uint64 expiration);
-
-//     /// @notice Renews the subscription to an NFT
-//     /// Throws if `tokenId` is not a valid NFT
-//     /// @param tokenId The NFT to renew the subscription for
-//     /// @param duration The number of seconds to extend a subscription for
-//     function renewSubscription(uint256 tokenId, uint64 duration)
-//         external
-//         payable;
-
-//     /// @notice Cancels the subscription of an NFT
-//     /// @dev Throws if `tokenId` is not a valid NFT
-//     /// @param tokenId The NFT to cancel the subscription for
-//     function cancelSubscription(uint256 tokenId) external payable;
-
-//     /// @notice Gets the expiration date of a subscription
-//     /// @dev Throws if `tokenId` is not a valid NFT
-//     /// @param tokenId The NFT to get the expiration date of
-//     /// @return The expiration date of the subscription
-//     function expiresAt(uint256 tokenId) external view returns (uint64);
-
-//     /// @notice Determines whether a subscription can be renewed
-//     /// @dev Throws if `tokenId` is not a valid NFT
-//     /// @param tokenId The NFT to get the expiration date of
-//     /// @return The renewability of a the subscription
-//     function isRenewable(uint256 tokenId) external view returns (bool);
-// }
--- test/manifest/ManifestV1.t.sol
+++ test/manifest/ManifestV1.t.sol
@@ -3,19 +3,20 @@ pragma solidity ^0.8.17;
 
 import "@forge/Test.sol";
 import "@forge/console2.sol";
-// import "../finance/CrowdFinancingV1/BaseCampaignTest.t.sol";
-// import "src/finance/CrowdFinancingV1.sol";
-// import "src/tokens/ERC20Token.sol";
 import "src/manifest/ManifestV1.sol";
 
 contract ManifestV1Test is Test {
     // error NoActiveSubscription(address account);
+    event SubscriptionFunded(address indexed account, uint256 tokensTransferred, uint256 timeRemaining);
+    event SubscriptionCanceled(address indexed account, uint256 tokensTransferred, uint256 timeReclaimed);
+    event CreatorWithdraw(address indexed account, uint256 tokensTransferred);
 
-      modifier prank(address user) {
+    modifier prank(address user) {
         vm.startPrank(user);
         _;
         vm.stopPrank();
     }
+
     address internal creator = 0xB4c79DAB8f259C7AEE6e5B2Aa729821764227E8A;
     address internal fees = 0xB4C79DAB8f259c7Aee6E5b2AA729821764227e7A;
     address internal alice = 0xb4c79DAB8f259c7Aee6E5b2aa729821864227E81;
@@ -25,22 +26,18 @@ contract ManifestV1Test is Test {
     ManifestV1 internal manifest;
 
     function purchase(address account, uint256 amount) internal prank(account) {
-      manifest.purchase{ value: amount }(amount);
+        manifest.purchase{value: amount}(amount);
     }
 
     function cancel(address account) internal prank(account) {
-      manifest.cancelSubscription();
+        manifest.cancelSubscription();
     }
 
     function setUp() public {
         manifest = new ManifestV1();
 
         vm.store(address(manifest), bytes32(uint256(0)), bytes32(0));
-        manifest.initialize(
-          creator,
-          "https://art.meow.com/",
-          2
-        );
+        manifest.initialize("Meow Manifest", "MEOW", "https://art.meow.com/", creator, 2, address(0));
 
         deal(alice, 1e19);
         deal(bob, 1e19);
@@ -49,95 +46,92 @@ contract ManifestV1Test is Test {
         deal(fees, 1e19);
     }
 
-    function testSub() public prank(alice) {
-      manifest.purchase{ value: 1e18 }(1e18);
-      assertEq(address(manifest).balance, 1e18);
-      assertEq(manifest.balanceOf(alice), 5e17);
-      (uint256 tokenId,,,) = manifest.subscriptionOf(alice);
-      assertEq(manifest.ownerOf(tokenId), alice);
+    function testPurchase() public prank(alice) {
+        vm.expectEmit(true, true, false, true, address(manifest));
+        emit SubscriptionFunded(alice, 1e18, 1e18 / 2 - block.timestamp);
+        manifest.purchase{value: 1e18}(1e18);
+        assertEq(address(manifest).balance, 1e18);
+        assertEq(manifest.timeBalanceOf(alice), 5e17);
+        (uint256 tokenId,,) = manifest.subscriptionOf(alice);
+        assertEq(manifest.ownerOf(tokenId), alice);
     }
 
     function testNonSub() public {
-      assertEq(manifest.balanceOf(alice), 0);
-      (uint256 tokenId,,,) = manifest.subscriptionOf(alice);
-      vm.expectRevert("ERC721: invalid token ID");
-      manifest.ownerOf(tokenId);
+        assertEq(manifest.timeBalanceOf(alice), 0);
+        (uint256 tokenId,,) = manifest.subscriptionOf(alice);
+        vm.expectRevert("ERC721: invalid token ID");
+        manifest.ownerOf(tokenId);
     }
 
-    function testSubDecay() public prank(alice) {
-      manifest.purchase{ value: 1e18 }(1e18);
-      vm.warp(block.timestamp + 25e16);
-      assertEq(manifest.balanceOf(alice), 5e17 / 2);
+    function testPurchaseDecay() public prank(alice) {
+        manifest.purchase{value: 1e18}(1e18);
+        vm.warp(block.timestamp + 25e16);
+        assertEq(manifest.timeBalanceOf(alice), 5e17 / 2);
     }
 
-    function testSubExpires() public prank(alice) {
-      manifest.purchase{ value: 1e18 }(1e18);
-      vm.warp(block.timestamp + 6e17);
-      assertEq(manifest.balanceOf(alice), 0);
-      (uint256 tokenId,,,) = manifest.subscriptionOf(alice);
-      vm.expectRevert("ERC721: invalid token ID");
-      manifest.ownerOf(tokenId);
+    function testPurchasExpire() public prank(alice) {
+        manifest.purchase{value: 1e18}(1e18);
+        vm.warp(block.timestamp + 6e17);
+        assertEq(manifest.timeBalanceOf(alice), 0);
+        (uint256 tokenId,,) = manifest.subscriptionOf(alice);
+        assertEq(manifest.ownerOf(tokenId), alice);
     }
 
-    function testSpacedPurchase() public {
-      purchase(alice, 1e18);
-      assertEq(manifest.balanceOf(alice), 5e17);
-      vm.warp(block.timestamp + 1e18);
-      assertEq(manifest.balanceOf(alice), 0);
-      purchase(alice, 1e18);
-      assertEq(manifest.balanceOf(alice), 5e17);
+    function testPurchaseSpaced() public {
+        purchase(alice, 1e18);
+        assertEq(manifest.timeBalanceOf(alice), 5e17);
+        vm.warp(block.timestamp + 1e18);
+        assertEq(manifest.timeBalanceOf(alice), 0);
+        purchase(alice, 1e18);
+        assertEq(manifest.timeBalanceOf(alice), 5e17);
     }
 
     function testCancel() public prank(alice) {
-      manifest.purchase{ value: 1e18 }(1e18);
-      vm.warp(block.timestamp + 1e17);
-      uint256 balance = alice.balance;
-      assertEq(manifest.balanceOf(alice), 4e17);
-      manifest.cancelSubscription();
-      assertEq(manifest.balanceOf(alice), 0);
-      assertEq(alice.balance, balance + 8e17);
-      vm.expectRevert("NoActiveSubscription");
-      manifest.cancelSubscription();
-    }
-
-    function testCancelMultiPrice() public {
-      purchase(alice, 1e18);
-      vm.startPrank(creator);
-      manifest.updatePrice(4);
-      vm.stopPrank();
-      purchase(alice, 1e18);
-      assertEq(manifest.balanceOf(alice), 5e17 + 25e16);
+        manifest.purchase{value: 1e18}(1e18);
+        vm.warp(block.timestamp + 1e17);
+        uint256 balance = alice.balance;
+        assertEq(manifest.timeBalanceOf(alice), 4e17);
+        vm.expectEmit(true, true, false, true, address(manifest));
+        emit SubscriptionCanceled(alice, 8e17, 4e17);
+        manifest.cancelSubscription();
+        assertEq(manifest.timeBalanceOf(alice), 0);
+        assertEq(alice.balance, balance + 8e17);
+        vm.expectRevert("NoActiveSubscription");
+        manifest.cancelSubscription();
     }
 
     function testCreatorEarnings() public {
-      purchase(alice, 1e18);
-      purchase(bob, 1e18);
-      purchase(charlie, 1e18);
-      assertEq(manifest.creatorBalance(), 0);
-      vm.warp(block.timestamp + 25e16);
-      assertEq(manifest.creatorEarnings(),  15e17);
-      vm.warp(block.timestamp + 25e16);
-      assertEq(manifest.creatorEarnings(),  3e18);
+        purchase(alice, 1e18);
+        purchase(bob, 1e18);
+        purchase(charlie, 1e18);
+        assertEq(manifest.creatorBalance(), 0);
+        vm.warp(block.timestamp + 25e16);
+        assertEq(manifest.creatorEarnings(), 15e17);
+        vm.warp(block.timestamp + 25e16);
+        assertEq(manifest.creatorEarnings(), 3e18);
     }
 
     function testCreatorWithdraw() public {
-      purchase(alice, 1e18);
-      purchase(bob, 1e18);
-      vm.warp(block.timestamp + 25e16);
+        purchase(alice, 1e18);
+        purchase(bob, 1e18);
+        vm.warp(block.timestamp + 25e16);
 
-      vm.startPrank(creator);
-      assertEq(manifest.creatorBalance(),  1e18);
-      manifest.withdraw();
-      assertEq(manifest.creatorBalance(),  0);
-      assertEq(manifest.creatorEarnings(),  1e18);
+        vm.startPrank(creator);
+        assertEq(manifest.creatorBalance(), 1e18);
 
-      vm.expectRevert("No Balance");
-      manifest.withdraw();
-      vm.stopPrank();
+        vm.expectEmit(true, true, false, true, address(manifest));
+        emit CreatorWithdraw(creator, 1e18);
+        manifest.withdraw();
+        assertEq(manifest.creatorBalance(), 0);
+        assertEq(manifest.creatorEarnings(), 1e18);
 
-      cancel(alice);
-      cancel(bob);
+        vm.expectRevert("No Balance");
+        manifest.withdraw();
+        vm.stopPrank();
 
-      assertEq(address(manifest).balance,  0);
+        cancel(alice);
+        cancel(bob);
+
+        assertEq(address(manifest).balance, 0);
     }
 }
